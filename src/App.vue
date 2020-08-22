0904095bb36c17eda2cc8b7665d3a442f6b6f2ee
bruno alfred <alfredybruno@gmail.com>
2020-08-22 09:53:35 +0300

    test the action

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to My Vue App"/>
+    <HelloWorld msg="Welcome to my Vue App"/>
   </div>
 </template>
 
