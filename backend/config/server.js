4da7ac21b4427e81a1b80da48751c4f58fdf2b46
brunoalfred <alfredybruno@gmail.com>
2021-04-24 17:00:15 +0300

    Port listening for the heroku App

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('process.env.PORT', 1337),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'e656c25e66f3e5fa1d2077b46e19b206'),
