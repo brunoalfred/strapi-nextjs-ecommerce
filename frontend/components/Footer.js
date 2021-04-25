bbf1e0bcb7f0119173a0c806ca7b03f6015e18d2
brunoalfred <alfredybruno@gmail.com>
2021-04-25 21:27:32 +0300

    Edited texts on the hompage

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -2,17 +2,17 @@ const Footer = () => {
   return (
     <div className="flex justify-between m-6">
       <p className="text-xs font-semibold text-gray-600">
-        Strapi Starter Next.js E-commerce
+        Made with ❤️ by [brunoalfred](https://github.com/brunoalfred)
       </p>
       <div className="flex gap-3 ml-4">
-        <a href="https://twitter.com/strapijs" className="max-w-xs ml-4">
+        <a href="https://twitter.com/brunoalfred" className="max-w-xs ml-4">
           <img src="/twitter.svg" alt="Twitter" />
         </a>
-        <a href="https://facebook.com/strapijs" className="ml-3">
+        <a href="#" className="ml-3">
           <img src="/facebook.svg" alt="Facebook" />
         </a>
         <a
-          href="https://github.com/strapi/strapi-starter-next-ecommerce"
+          href="https://github.com/brunoalfred"
           className="ml-3"
         >
           <img src="/github.svg" alt="GitHub" />
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,7 +6,7 @@ const HomePage = ({ products }) => {
   return (
     <div>
       <Head>
-        <title>Strapi Next.js E-commerce</title>
+        <title>Stickers - Store</title>
       </Head>
       <ProductsList products={products} />
     </div>
